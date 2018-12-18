7fd2c6fa5823eda070a6a07da6f702e835ac55eb
mkolosick <matt@kolosick.com>
2018-12-17 16:32:00 -0800

    Initial splitting up of testing framework. Moved all testing implementation to tests directory. Integration tests are now managed through tests/Integration/Tests.hs

---
+++ tests/Integration/Tests.hs
@@ -0,0 +1,64 @@
+module Integration.Tests (integrationTests) where
+
+import Control.Monad (liftM3)
+import Data.Bool (bool)
+
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.IO.Error
+import System.IO
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Language.Mist.Runner
+
+integrationTests = group "Integration tests"
+  [ testGroup "pos" <$> dirTests "tests/Integration/pos"    ExitSuccess
+  , testGroup "neg" <$> dirTests "tests/Integration/neg"    (ExitFailure 1)
+  ]
+
+
+---------------------------------------------------------------------------
+dirTests :: FilePath -> ExitCode -> IO [TestTree]
+---------------------------------------------------------------------------
+dirTests root code = do
+  files    <- walkDirectory root
+  let tests = [ rel | f <- files, isTest f, let rel = makeRelative root f ]
+  return    $ mkTest code root <$> tests
+
+isTest   :: FilePath -> Bool
+isTest f = takeExtension f `elem` [".hs"]
+
+---------------------------------------------------------------------------
+mkTest :: ExitCode -> FilePath -> FilePath -> TestTree
+---------------------------------------------------------------------------
+mkTest code dir file = testCase file $ do
+  createDirectoryIfMissing True $ takeDirectory log
+  withFile log WriteMode $ \h -> do
+    ec <- runMist h test
+    assertEqual "Wrong exit code" code (resultExitCode ec)
+  where
+    test = dir </> file
+    log  = let (d, f) = splitFileName file in dir </> d </> ".liquid" </> f <.> "log"
+
+-- resultExitCode :: Result -> ExitCode
+resultExitCode (Left _)  = ExitFailure 1
+resultExitCode (Right _) = ExitSuccess
+
+----------------------------------------------------------------------------------------
+-- Generic Helpers
+----------------------------------------------------------------------------------------
+
+group n xs = testGroup n <$> sequence xs
+
+----------------------------------------------------------------------------------------
+walkDirectory :: FilePath -> IO [FilePath]
+----------------------------------------------------------------------------------------
+walkDirectory root =
+  fmap concat . traverse collect . candidates
+    =<< (getDirectoryContents root `catchIOError` const (return []))
+  where
+    candidates fs = [root </> f | f <- fs, not (isExtSeparator (head f))]
+    collect f = liftM3 bool (pure [f]) (walkDirectory f) (doesDirectoryExist f)
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,4 +1,53 @@
-import Language.Mist.Test 
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Main (main) where
+
+import Test.Tasty
+import Test.Tasty.Ingredients.Rerun
+import Test.Tasty.Runners
+import Test.Tasty.Runners.AntXML
+
+import Integration.Tests
 
 main :: IO ()
-main = runTests 
+main = runTests
+
+runTests :: IO ()
+runTests = run =<< group "Tests" [integrationTests]
+  where
+    run = defaultMainWithIngredients [
+                testRunner
+            --  , includingOptions [ Option (Proxy :: Proxy NumThreads)
+            --                     , Option (Proxy :: Proxy LiquidOpts)
+            --                     , Option (Proxy :: Proxy SmtSolver) ]
+              ]
+
+testRunner :: Ingredient
+testRunner = rerunningTests
+               [ listingTests
+               , combineReporters myConsoleReporter antXMLRunner
+               , myConsoleReporter
+               ]
+
+myConsoleReporter :: Ingredient
+myConsoleReporter = consoleTestReporter
+
+-- | Combine two @TestReporter@s into one.
+--
+-- Runs the reporters in sequence, so it's best to start with the one
+-- that will produce incremental output, e.g. 'consoleTestReporter'.
+combineReporters :: Ingredient -> Ingredient -> Ingredient
+combineReporters (TestReporter opts1 run1) (TestReporter opts2 run2)
+  = TestReporter (opts1 ++ opts2) $ \opts tree -> do
+      f1 <- run1 opts tree
+      f2 <- run2 opts tree
+      return $ \smap -> f1 smap >> f2 smap
+combineReporters _ _ = error "combineReporters needs TestReporters"
+
+----------------------------------------------------------------------------------------
+-- Generic Helpers
+----------------------------------------------------------------------------------------
+
+group n xs = testGroup n <$> sequence xs
