1493b7cce590a79ffc9d6f0131c3611254ac99b8
Anish Tondwalkar <anish@archlinux.info>
2018-12-18 15:09:15 -0800

    cleanup Fresh

--- lib/Language/Mist/Fresh.hs
+++ lib/Language/Mist/Fresh.hs
@@ -4,48 +4,60 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Language.Mist.Fresh
-  (
-  ) where
+module Language.Mist.Fresh (
+     uniqify, uniqifyRType,
+     varNum,
+
+     FreshT (..),
+     Fresh,
+
+     runFreshT, execFreshT,
+
+     ) where
 
 import Control.Monad.State
-import qualified Data.Map.Strict as M
-import Data.Maybe (fromJust)
-import Language.Mist.Types
-import Language.Mist.Utils (safeTail)
 import Control.Applicative (Alternative)
 import Control.Monad.Identity (Identity, runIdentity)
+-- import Control.Monad.Trans.Maybe
+import qualified Data.Map.Strict as M
+import Data.Maybe (fromJust)
+import Data.List.Split (splitOn)
 
--- | TODO: should this separate type variables and term variables?
+import Language.Mist.Types
+import Language.Mist.Utils (safeTail)
+
+cSEPARATOR = "##"
+varNum :: Id -> Int
+varNum = read . last . splitOn cSEPARATOR
+createInternalName name number = name ++ cSEPARATOR ++ show number
+
+
+--------------------------------------------------------------------------------
 -- | A MonadFresh encompasses the operations for generating fresh, scoped names
-class Monad m => MonadFresh a m where
-  genFreshName :: a -> m a             -- ^ generate a fresh name for the argument
-  popScopedName :: a -> m ()           -- ^ removes the most recent scoped version of a fresh name
-  getAssignedName :: a -> m (Maybe a)  -- ^ gets the assigned fresh name
+--------------------------------------------------------------------------------
+class Monad m => MonadFresh m where
+  refreshId :: Id -> m Id         -- ^ generate a fresh name for the argument
+  popId     :: Id -> m ()         -- ^ removes the most recent scoped version of a fresh name
+  lookupId  :: Id -> m (Maybe Id) -- ^ gets the assigned fresh name
 
-
-createInternalName :: Id -> Integer -> Id
-createInternalName name number = name ++ "##" ++ show number
-
+--------------------------------------------------------------------------------
 -- Using a global (for all names) freshInt means we can pick up later
 -- and not run into name collisions without having to refind all existing names.
-data FreshState = FreshState { newNameMap :: M.Map Id [Id], freshInt :: Integer }
+--------------------------------------------------------------------------------
+data FreshState = FreshState { nameMap :: M.Map Id [Id], freshInt :: Integer }
 
 newtype FreshT m a = FreshT { unFreshT :: StateT FreshState m a }
  deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadTrans)
 
-newtype Fresh a = Fresh { unFresh :: FreshT Identity a }
- deriving (Functor, Applicative, Monad)
+type Fresh = FreshT Identity
 
-instance MonadFresh Id Fresh where
-  genFreshName = Fresh . genFreshId
-  popScopedName = Fresh . popScopedId
-  getAssignedName = Fresh . getAssignedId
+instance Monad m => MonadFresh (FreshT m) where
+  refreshId = genFreshId
+  popId     = popScopedId
+  lookupId  = getAssignedId
 
 emptyFreshState :: FreshState
-emptyFreshState = FreshState { newNameMap = M.empty
-                             , freshInt = 0
-                             }
+emptyFreshState = FreshState { nameMap = M.empty, freshInt = 0 }
 
 -- | pushes a new name onto the name stack
 pushName :: Id -> Id -> M.Map Id [Id] -> M.Map Id [Id]
@@ -58,20 +70,20 @@ genFreshId name = do
   FreshState { freshInt = currentFreshInt } <- FreshT get
   let newFreshInt = currentFreshInt + 1
       newName = createInternalName name currentFreshInt
-  FreshT (modify $ \freshState@FreshState { newNameMap = newNameMap } ->
+  FreshT (modify $ \freshState@FreshState { nameMap = nameMap } ->
                      freshState { freshInt = newFreshInt
-                                , newNameMap = pushName name newName newNameMap
+                                , nameMap = pushName name newName nameMap
                                 })
   return newName
 
 popScopedId :: (Monad m) => Id -> FreshT m ()
 popScopedId name =
-  FreshT (modify $ \freshState@FreshState { newNameMap = newNameStack } ->
-             freshState { newNameMap = M.adjust safeTail name newNameStack })
+  FreshT (modify $ \freshState@FreshState { nameMap = newNameStack } ->
+             freshState { nameMap = M.adjust safeTail name newNameStack })
 
 getAssignedId :: (Monad m) => Id -> FreshT m (Maybe Id)
 getAssignedId name =
-  FreshT (gets $ fmap head . M.lookup name . newNameMap)
+  FreshT (gets $ fmap head . M.lookup name . nameMap)
 
 
 -- | Run a computation with a fresh name supply.
@@ -85,92 +97,91 @@ evalFreshT (FreshT m) initialNames = evalStateT m initialNames
 execFreshT :: Monad m => FreshT m a -> FreshState -> m FreshState
 execFreshT (FreshT m) initialNames = execStateT m initialNames
 
-evalFresh :: Fresh a -> FreshState -> a
-evalFresh (Fresh s) initialNames = runIdentity $ evalFreshT s initialNames
-
+--------------------------------------------------------------------------------
 
 uniqify :: Expr a -> Expr a
-uniqify expr = evalFresh (uniqifyExpr expr) emptyFreshState
+uniqify expr = runIdentity $ evalFreshT (uniqifyExpr expr) emptyFreshState
 
-uniqifyExpr :: (MonadFresh Id m) => Expr a -> m (Expr a)
+uniqifyExpr :: (MonadFresh m) => Expr a -> m (Expr a)
+uniqifyExpr (Lam binds body extra) = do
+  (newBinds, oldNames) <- unzip <$> mapM uniqifyBind binds
+  newLam <- Lam newBinds <$> uniqifyExpr body <*> pure extra
+  mapM_ popId (reverse oldNames)
+  return newLam
+uniqifyExpr (Let bind sig e1 e2 extra) = do
+  (newBind, oldName) <- uniqifyBind bind
+  newLet <- Let newBind <$> uniqifySig sig <*> uniqifyExpr e1 <*> uniqifyExpr e2 <*> pure extra
+  popId oldName
+  return newLet
+uniqifyExpr (Id id extra) = do
+  assignedName <- fmap fromJust $ lookupId id
+  return $ Id assignedName extra
+
 uniqifyExpr e@Number{} = pure e
 uniqifyExpr e@Boolean{} = pure e
-uniqifyExpr (Id id extra) = do
-  assignedName <- fmap fromJust $ getAssignedName id
-  return $ Id assignedName extra
+uniqifyExpr e@Unit{} = pure e
 uniqifyExpr (Prim2 op e1 e2 extra) =
   Prim2 op <$> uniqifyExpr e1 <*> uniqifyExpr e2 <*> pure extra
 uniqifyExpr (If e1 e2 e3 extra) =
   If <$> uniqifyExpr e1 <*> uniqifyExpr e2 <*> uniqifyExpr e3 <*> pure extra
-uniqifyExpr (Let bind sig e1 e2 extra) = do
-  (newBind, oldName) <- uniqifyBind bind
-  newLet <- Let newBind <$> uniqifySig sig <*> uniqifyExpr e1 <*> uniqifyExpr e2 <*> pure extra
-  popScopedName oldName
-  return newLet
 uniqifyExpr (Tuple e1 e2 extra) =
   Tuple <$> uniqifyExpr e1 <*> uniqifyExpr e2 <*> pure extra
 uniqifyExpr (GetItem e field extra) =
   GetItem <$> uniqifyExpr e <*> pure field <*> pure extra
 uniqifyExpr (App e1 e2 extra) =
   App <$> uniqifyExpr e1 <*> uniqifyExpr e2 <*> pure extra
-uniqifyExpr (Lam binds body extra) = do
-  (newBinds, oldNames) <- unzip <$> mapM uniqifyBind binds
-  newLam <- Lam newBinds <$> uniqifyExpr body <*> pure extra
-  mapM_ popScopedName (reverse oldNames)
-  return newLam
-uniqifyExpr e@Unit{} = pure e
 
-uniqifySig :: (MonadFresh Id m) => Sig a -> m (Sig a)
+uniqifySig :: (MonadFresh m) => Sig a -> m (Sig a)
 uniqifySig Infer = pure Infer
 uniqifySig (Check r) = Check <$> uniqifyRPoly r
 uniqifySig (Assume r) = Assume <$> uniqifyRPoly r
 
-uniqifyRPoly :: (MonadFresh Id m) => RPoly a -> m (RPoly a)
+uniqifyRPoly :: (MonadFresh m) => RPoly Expr a -> m (RPoly Expr a)
 uniqifyRPoly (RForall tvars r) = do
   (newTvars, oldNames) <- unzip <$> mapM uniqifyBindingTVar tvars
   newRForall <- RForall newTvars <$> uniqifyRType r
-  mapM_ popScopedName (reverse oldNames)
+  mapM_ popId (reverse oldNames)
   return newRForall
 
-uniqifyRType :: (MonadFresh Id m) => RType a -> m (RType a)
+uniqifyRType :: (MonadFresh m) => RType Expr a -> m (RType Expr a)
 uniqifyRType (RBase bind typ expr) = do
   (newBind, oldName) <- uniqifyBind bind
   newRBase <- RBase newBind <$> uniqifyType typ <*> uniqifyExpr expr
-  popScopedName oldName
+  popId oldName
   return newRBase
-unqifyRType (RFun bind rtype1 rtype2) = do
+uniqifyRType (RFun bind rtype1 rtype2) = do
   (newBind, oldName) <- uniqifyBind bind
   newRFun <- RFun newBind <$> uniqifyRType rtype1 <*> uniqifyRType rtype2
-  popScopedName oldName
+  popId oldName
   return newRFun
-unqifyRType (RRTy bind rtype expr) = do
+uniqifyRType (RRTy bind rtype expr) = do
   (newBind, oldName) <- uniqifyBind bind
   newRRTy <- RRTy newBind <$> uniqifyRType rtype <*> uniqifyExpr expr
-  popScopedName oldName
+  popId oldName
   return newRRTy
 
-uniqifyType :: (MonadFresh Id m) => Type -> m Type
+uniqifyType :: (MonadFresh m) => Type -> m Type
 uniqifyType (TVar tvar) = TVar <$> uniqifyTVar tvar
-uniqifyTyp TInt = pure TInt
-uniqifyTyp TBool = pure TBool
-uniqifyTyp (domain :=> codomain) =
-  (:=>) <$> mapM uniqifyTyp domain <*> uniqifyTyp codomain
-uniqifyTyp (TPair t1 t2) =
-  TPair <$> uniqifyTyp t1 <*> uniqifyTyp t2
-uniqifyTyp (TCtor c ts) =
-  TCtor c <$> mapM uniqifyTyp ts
+uniqifyType TInt = pure TInt
+uniqifyType TBool = pure TBool
+uniqifyType (domain :=> codomain) =
+  (:=>) <$> mapM uniqifyType domain <*> uniqifyType codomain
+uniqifyType (TPair t1 t2) =
+  TPair <$> uniqifyType t1 <*> uniqifyType t2
+uniqifyType (TCtor c ts) =
+  TCtor c <$> mapM uniqifyType ts
 
 -- | returns the new binding and the original name
-uniqifyBind :: (MonadFresh Id m) => Bind a -> m (Bind a, Id)
+uniqifyBind :: (MonadFresh m) => Bind a -> m (Bind a, Id)
 uniqifyBind (Bind name extra) = do
-  newName <- genFreshName name
+  newName <- refreshId name
   return (Bind newName extra, name)
 
 -- | returns the new binding and the original name
-uniqifyBindingTVar :: (MonadFresh Id m) => TVar -> m (TVar, Id)
+uniqifyBindingTVar :: (MonadFresh m) => TVar -> m (TVar, Id)
 uniqifyBindingTVar (TV name) = do
-  newName <- genFreshName name
+  newName <- refreshId name
   return (TV newName, name)
 
-uniqifyTVar :: (MonadFresh Id m) => TVar -> m TVar
-uniqifyTVar (TV name) = TV <$> (fromJust <$> getAssignedName name)
+uniqifyTVar :: (MonadFresh m) => TVar -> m TVar
+uniqifyTVar (TV name) = TV <$> (fromJust <$> lookupId name)
