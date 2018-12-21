7f9460b5fa269f9b071070f39f8ef2d7e306e7ec
mkolosick <matt@kolosick.com>
2018-12-21 15:32:55 -0800

    Curried everything

--- lib/Language/Mist/CGen.hs
+++ lib/Language/Mist/CGen.hs
@@ -3,8 +3,10 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 module Language.Mist.CGen ( generateConstraints ) where
+
 -- TODO: Do we need to run a Uniqify pass before we run this module?
 -- Matt: We should uniquify at the beginning and then maintain the unique names property
+
 import           Language.Mist.Types
 import           Language.Mist.Names
 import           Language.Mist.Checker (prim2Unpoly)
@@ -13,7 +15,7 @@ import           Control.Monad.State.Strict
 
 data SubC a = SubC [(Id, RType Core a)] (RType Core a) (RType Core a)
   deriving Show
-data CGInfo a = CGInfo { subCs :: [SubC a], fresh :: Int }
+data CGInfo a = CGInfo { subCs :: [SubC a] }
   deriving Show
 
 type CG a = StateT (CGInfo a) Fresh
@@ -36,11 +38,8 @@ instance Monoid (CGInfo a) where
 addC :: CGEnv a -> RType Core a -> RType Core a -> CG a ()
 addC γ t t' = modify $ \(CGInfo scs) -> CGInfo (SubC γ t t':scs)
 
-addBinds :: [AnnBind a] -> CGEnv a -> CGEnv a
-addBinds = flip (foldr addB)
-
-addB :: AnnBind a -> CGEnv a -> CGEnv a
-addB (AnnBind x t _) γ = (x, t) : γ
+addBind :: AnnBind a -> CGEnv a -> CGEnv a
+addBind (AnnBind x t _) γ = (x, t) : γ
 
 generateConstraints :: Core a -> CGInfo a
 generateConstraints = runFresh . flip execStateT mempty . synth []
@@ -54,19 +53,18 @@ synth _ e@(CPrim2 o _ _ _) = prim e $ prim2Unpoly o
 synth γ (CId x _   ) = single γ x
 
 synth γ (CApp f y _) = do
-  RForall [] (RFun x t t') <- synth γ f
+  RFun x t t' <- synth γ f
   -- TODO: Enforce this in the refinement type
   let CId y' _ = y
-  addC γ <$> single γ y' <*> pure (RForall [] t)
-  pure $ RForall [] $ subst1 y (bindId x) t'
+  addC γ <$> single γ y' <*> pure t
+  pure $ subst1 y (bindId x) t'
 
-synth γ (CTAbs as e _) = do
-  RForall as' t <- synth γ e
-  pure $ RForall (as' ++ as) t
+synth γ (CTAbs a e _) = do
+  RForall a <$> synth γ e
 
 synth γ (CTApp e tau _) = do
-  RForall (TV a : as) t <- synth γ e
-  pure $ RForall as $ subst1 tau a t
+  RForall (TV a) t <- synth γ e
+  pure $ subst1 tau a t
 
   -- Fake ADT stuff
 synth _γ (CTuple _e1 _e2 _) = undefined
@@ -75,26 +73,26 @@ synth _γ (CIf _b _e1 _e2 _) = undefined
 
 -- "Bidirectional" "portal" that's made redudant by the fact that we insert
 -- all the KVARs at parse time
-synth γ (CLam xs e _)
-  = bindsRType xs <$>
-    synth (addBinds xs γ) e
+synth γ (CLam x e _)
+  = bindsRType x <$>
+    synth (addBind x γ) e
 synth γ  (CLet b@(AnnBind _ t1 _) e1 e2 _)
   = synth γ e1 >>=
     flip (addC γ) t1 >>
-    synth (addB b γ) e2
+    synth (addBind b γ) e2
 
 prim :: Core a -> Type -> CG a (RType Core a)
-prim e t = refresh $ RForall [] $ RBase vv t expr
-  where l = extractC e
-        vv = Bind "VV" l
-        expr = CPrim2 Equal (CId "VV" l) e l
+prim e t = do
+  let l = extractC e
+  vv <- refreshId "VV"
+  let expr = CPrim2 Equal (CId vv l) e l
+  pure $ RBase (Bind vv l) t expr
 
 single :: CGEnv a -> Id -> CG a (RType Core a)
 single γ x = case lookup x γ of
-  -- TODO: why do we need to refresh this? Just blindly following the paper
-  -- here
-  Just rt@(RForall _ (RBase (Bind v l) _ _))
-   | v == x -> do rt'@(RForall [] (RBase (Bind v' _) _ _)) <- refresh rt
+  Just rt@(RBase (Bind v l) _ _)
+   | v == x -> do v' <- refreshId "VV"
+                  let rt' = subst1 (CId v' l) x rt
                   pure $ strengthen (CPrim2 Equal (CId v' l) (CId x l) l) rt'
   Just rt -> pure rt
   Nothing -> error $ "Unbound Variable " ++ show x
