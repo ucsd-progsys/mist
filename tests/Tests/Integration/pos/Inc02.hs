04fe8a977aef8ef76c174382c98969a03d61bf41
Ranjit Jhala <jhala@cs.ucsd.edu>
2019-01-11 14:29:42 -0800

    testS

---
+++ tests/Tests/Integration/pos/Inc02.hs
@@ -0,0 +1,12 @@
+
+incr :: x:Int -> {v:Int | v == x + 1} 
+incr = ( \x -> x + 1 ) 
+
+moo :: {v:Int | v == 8} 
+moo =  ( incr 7 )
+
+id   :: a -> a
+id x = x 
+
+bar :: {v:Int | v == 8} 
+bar =  incr (id 7)
