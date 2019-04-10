adbba40c38f2ad41d2e6da8fa28897e52a669b66
Anish Tondwalkar <anish@archlinux.info>
2019-04-10 13:24:07 -0700

    Added tycon tests

---
+++ tests/Tests/Integration/pos/incrState.hs
@@ -0,0 +1,3 @@
+ret as forall a. w:Int ~> x:a -> ST <{v:Int|v=w} >Int >a
+-- ret :: w:Int ~> a -> ST <{v:Int|v=w} >{v:Int|v=w} >a
+ret = (0)
---
+++ tests/Tests/Integration/pos/pair.hs
@@ -0,0 +1,5 @@
+fst as forall a, b. (Pair >a >b) -> a
+fst = True
+snd as forall a, b. (Pair >a >b) -> b
+snd = True
+
