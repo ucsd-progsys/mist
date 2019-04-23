41444b0f361cab31a5a397895a7eb09dd131e769
Anish Tondwalkar <anish@archlinux.info>
2019-04-23 09:07:29 -0700

    Update testcases
    - pair now tests polymorphic function application (and fails in
    checker)
    - incrState no longer uses 0-ary get

--- tests/Tests/Integration/pos/incrState.hs
+++ tests/Tests/Integration/pos/incrState.hs
@@ -5,11 +5,11 @@ bind as forall s, a, b. w1:Int ~> w2:Int ~> w3:Int ~> (ST <{v:Int|v==w1} >{v:Int
   -> (unused:a -> ST <{v:Int|v==w2} >{v:Int|v==w3} >b)
   -> ST <{v:Int|v==w1} >{v:Int|v==w3} >b
 bind = (0)
-get as w:Int ~> ST <{v:Int|v==w} >{v:Int|v==w} >{v:Int|v==w}
+get as w:Int ~> Int -> ST <{v:Int|v==w} >{v:Int|v==w} >{v:Int|v==w}
 get = (0)
 put as w:Int -> ST <Int >{v:Int|v==w} >Unit
 put  = (0)
 
 -- incr
 incr :: ST <Int >Int >Unit
-incr = (bind get (\n -> (put n+1)))
+incr = (bind (get 0) (\n -> (put n+1)))
--- tests/Tests/Integration/pos/pair.hs
+++ tests/Tests/Integration/pos/pair.hs
@@ -2,4 +2,7 @@ fst as forall a, b. (Pair >a >b) -> a
 fst = True
 snd as forall a, b. (Pair >a >b) -> b
 snd = True
-
+mkPair as forall a, b. x:a -> y:b -> Pair >a >b
+mkPair = True
+foo :: {v:Int | v == 1}
+foo = (fst (mkPair 1 1))
