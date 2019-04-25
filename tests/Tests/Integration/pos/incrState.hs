4d368ea48dc18e25aa319f677673db9201d4b35a
mkolosick <matt@kolosick.com>
2019-04-25 14:40:32 -0700

    Whitespace and new test

--- tests/Tests/Integration/pos/incrState.hs
+++ tests/Tests/Integration/pos/incrState.hs
@@ -1,14 +1,17 @@
 -- Monadic Interface
 ret as forall a. w:Int ~> x:a -> ST <{v:Int|v==w} >{v:Int|v==w} >a
 ret = (0)
+
 bind as forall s, a, b. w1:Int ~> w2:Int ~> w3:Int ~> (ST <{v:Int|v==w1} >{v:Int|v==w2} >a)
   -> (unused:a -> ST <{v:Int|v==w2} >{v:Int|v==w3} >b)
   -> ST <{v:Int|v==w1} >{v:Int|v==w3} >b
 bind = (0)
+
 get as w:Int ~> Int -> ST <{v:Int|v==w} >{v:Int|v==w} >{v:Int|v==w}
 get = (0)
+
 put as w:Int -> ST <Int >{v:Int|v==w} >Unit
-put  = (0)
+put = (0)
 
 -- incr
 incr :: ST <Int >Int >Unit
