876cfaee107387f0eb5abe3d56ce60272a5969d7
mkolosick <matt@kolosick.com>
2019-07-02 15:41:45 -0700

    Updated pretty-print to include cubifying

--- scripts/pretty-print-horn.rkt
+++ scripts/pretty-print-horn.rkt
@@ -46,4 +46,43 @@
 (define (pretty-print formula)
   (apply string-append (pretty-print-depth formula 0)))
 
-(display (pretty-print (read)))
+(define (cubify constraints)
+  ;; EHC -> (LinearEHC -> LinearEHC) -> List LinearEHC
+  (define (cubify-help formula path-builder)
+    (match formula
+      [(list 'and conjuncts ...)
+       (apply append (map (lambda (conjunct) (cubify-help conjunct path-builder)) conjuncts))]
+
+      [(list 'forall head body)
+       (cubify-help body (lambda (x) (path-builder (list 'forall head x))))]
+
+      [(list 'exists head body)
+       (cubify-help body (lambda (x) (path-builder (list 'exists head x))))]
+
+      [formula (list (path-builder formula))]))
+
+  (cubify-help constraints identity))
+
+(define input-file (make-parameter #f))
+(define hypercube (make-parameter #f))
+
+(command-line
+ #:program "pretty-print"
+ #:once-each
+ [("-i" "--input") file-name
+                   "read input from file"
+                   (input-file file-name)]
+ [("--hypercube") "print the constraints as a hyper cube"
+                  (hypercube #t)])
+
+
+(current-input-port (if (input-file)
+                        (open-input-file (input-file))
+                        (current-input-port)))
+(define constraints (read))
+
+(displayln (pretty-print constraints))
+
+(when (hypercube)
+  (displayln "")
+  (for-each displayln (cubify constraints)))
