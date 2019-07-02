09b6bd6bfbf8438c9894a7eed38f6c24b282d217
mkolosick <matt@kolosick.com>
2019-07-02 15:41:29 -0700

    Pseudo mist linear examples

---
+++ tests/Tests/Integration/todo/linearTypes.hs
@@ -0,0 +1,23 @@
+{-
+
+data L = Fun Int L
+       | App L L
+       | Var Int
+
+type Lin env = Lin L
+
+var as n:Int -> Lin {v | v = singleton n}
+
+fun as env ~> n:Int -> Lin {v | v = env - (singleton n)} -> Lin {v | v = env}
+
+app as env ~> env' -> Lin {v | v = env} -> Lin {v | v = env'} -> Lin {v | v = union env env'}
+
+typecheck as Lin {v | v = emptyset} -> L
+
+term :: L
+term = (app (fun 1 (var 1))) (app (fun 1 (var 1)))
+
+badTerm :: L
+badTerm = (fun 1 (app (var 1) (var 1)))
+
+-}
