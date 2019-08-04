0c3f0967e1068769f8b509e6f974d46169a65f69
Anish Tondwalkar <anish@archlinux.info>
2019-08-03 22:30:22 -0700

    oops

--- tests/Tests/Integration/todo/tabs_with_ifun.hs
+++ tests/Tests/Integration/todo/tabs_with_ifun.hs
@@ -1,11 +1,11 @@
 set as h:(Map <Int >Int) ~> p:Int -> v:Int -> ST <{hg:Map <Int >Int | h == hg} >{hg:Map <Int >Int | store h p v == hg} >Unit
-set = undefined
+set = 0
 
 
 -----------------------------------------------------------------------------
 -- this works
-setprime :: h:(Map <Int >Int) ~> p:Int -> v:Int -> ST <{hg:Map <Int >Int | h == hg} >{hg:Map <Int >Int | store h p v == hg} >Unit
-setprime = \p -> \n -> set p n
+setprimeprime :: h:(Map <Int >Int) ~> p:Int -> v:Int -> ST <{hg:Map <Int >Int | h == hg} >{hg:Map <Int >Int | store h p v == hg} >Unit
+setprimeprime = \p -> \n -> set p n
 
 --- but this doesn't
 setprime :: forall a. h:(Map <Int >Int) ~> p:Int -> v:Int -> (z:a -> a) -> ST <{hg:Map <Int >Int | h == hg} >{hg:Map <Int >Int | store h p v == hg} >Unit
